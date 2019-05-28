cf1afce7d1f27a9c07000126f9a330eca7d4e533
anas ameziane <anas.ameziane@gmail.com>
2019-05-28 16:53:28 +0200

    add features

--- src/index.js
+++ src/index.js
@@ -39,3 +39,14 @@ function fadeWords(quotewords) {
 }
 
 splitWords();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      block: "start", 
+      behavior: 'smooth'
+    });
+  });
+});
